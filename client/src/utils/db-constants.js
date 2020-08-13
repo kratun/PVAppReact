d16d2d47850fa6eef85d11f5ed8f41da52cbc02b
kratun <mitevdv@abv.bg>
2020-08-13 22:33:01 +0300

    Add database settings file

---
+++ client/src/utils/db-constants.js
@@ -0,0 +1,5 @@
+export const API_AUTH_BASE_URL = '' // URL used for Register and Login in firebase
+
+export const API_BASE_URL = '' // URL used to access DB in firebase
+
+export const API_KEY = '' // Firebase Api KEY
